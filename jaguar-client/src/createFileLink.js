cac91974e58232b1e26b032f0125e1222b10c93c
Joshua Cook <josh.lsqd@gmail.com>
2018-04-28 10:10:29 -0700

    change uri6

--- jaguar-client/src/createFileLink.js
+++ jaguar-client/src/createFileLink.js
@@ -56,7 +56,7 @@ export default ({ uri, includeExtensions, ...requestOptions } = {}) => {
                 headers,
                 credentials,
                 fetchOptions = {},
-                uri: contextURI,
+                uri,
                 http: httpOptions = {},
             } = operation.getContext();
             const {
@@ -112,7 +112,7 @@ export default ({ uri, includeExtensions, ...requestOptions } = {}) => {
             }
             if (headers) fetcherOptions.headers = { ...fetcherOptions.headers, ...headers };
 
-            fetcher(contextURI || uri, fetcherOptions)
+            fetcher(uri, fetcherOptions)
             // attach the raw response to the context for usage
                 .then((response) => {
                     operation.setContext({ response });
