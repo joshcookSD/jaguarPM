a95d0724ccdf8d5554477f4b2c3e8327b1b808c9
Bryan <bryanburke91@gmail.com>
2018-04-28 08:54:52 -0700

    updated the face

--- jaguar-client/src/containers/layout/OrgAdminHeader.js
+++ jaguar-client/src/containers/layout/OrgAdminHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Query } from "react-apollo";
 import styled from 'styled-components';
-import { Image, Tab, Header, Card} from 'semantic-ui-react';
+import { Image, Tab, Header, Card, List, Label} from 'semantic-ui-react';
 import decode from 'jwt-decode';
 import { getOrgByOwner } from "../apollo-graphql/userQueries";
 import TeamForm from '../org_team/TeamForm';
@@ -34,12 +34,11 @@ const OrgAdminHeader = ({ owner }) => (
                 menuItem: org.orgtitle, render: () =>
                     <Tab.Pane className="orgTab" attached={false} >
                             <div className="orgHeader">
-                            <Header as='h3' block>
-                                <h3 className="topOrgLabel">Organization Title:</h3> {org.orgtitle}<br />
-                                <h3>Organization Description:</h3> {org.orgdescription}
-                                </Header>
+                            <div><span className="headerTitle " >Organization Title: </span>
+                                <span className="headerName">{org.orgtitle}</span></div>
+                            <div className="orgDescrip"><span className="headerTitle" >Organization Description: </span>
+                                <span className="headerName">{org.orgdescription}</span></div> 
                             </div> 
-
                             <div className="formTeamDiv">
                                 <div className="teamFormTeamInfo">
                                 <Card className="cardLeft">
@@ -47,10 +46,27 @@ const OrgAdminHeader = ({ owner }) => (
                                         <div className="teamInfo">
                                             <h3 className="orgTeamTitle">Team Info</h3>
                                             {  org.teams.map(team => (
-                                                <ul>
-                                                    <li> Team title {team.teamtitle}</li>
-                                                    <li>Teamdescription {team.teamdescription}</li>
-                                                </ul>
+                                                // <ul>
+                                                //     <li> <span>Team title</span><br></br> {team.teamtitle}</li>
+                                                // <li><span>Teamdescription</span><br></br> {team.teamdescription}</li>
+                                                // </ul>
+                                            <Card>
+                                                <Card.Content>
+                                                    <Card.Header>{team.teamtitle}</Card.Header>
+                                                    <Card.Meta>Title</Card.Meta>
+                                                    <Card.Description>{team.teamdescription}</Card.Description>
+                                                    {/* <Card.Meta>Description</Card.Meta> */}
+                                                </Card.Content>
+                                            </Card>
+
+                                            // <List>
+                                            //     <List.Item>
+                                            //         <List.Content>
+                                            //             <List.Header as='a'><List.Icon name='user' />{user.username}</List.Header>
+                                            //             {/* <List.Description>{team.projectdescription}</List.Description> */}
+                                            //         </List.Content>
+                                            //     </List.Item>
+                                            // </List>
                                             ))}
                                         </div>
                                      </Card>
@@ -59,12 +75,34 @@ const OrgAdminHeader = ({ owner }) => (
                                 <div className="userDropDownOrgList">
                                 <Card className="cardRight">
                                             <div className="currentUserOrgList"> 
-                                                <h3 className="orgCardTitle">Org Users</h3>  
+                                                <h3 className="orgCardTitle">Add User To Organization</h3>  
                                     <DropdownSelection orgId={org._id} getOrgByOwner={getOrgByOwner} variables={variables} />
                                                 {org.users.map(user => (
-                                                        <ul className="orgUsers">
-                                                        <li>{user.username}</li>
-                                                        </ul>
+                                                        // <ul className="orgUsers">
+                                                        // <li>{user.username}</li>
+                                                        // </ul>
+                                            // <List>
+                                            //     <List.Item>
+                                            //         <List.Content>
+                                            //             {/* <List.Header as='a'><List.Icon name='user' />{user.username}</List.Header> */}
+                                            //             <Label as='a' image>
+                                            //                 <List.Icon name='user' />
+                                            //                 {/* <img src='/assets/images/avatar/small/joe.jpg' /> */}
+                                            //                 {user.username}
+                                            //                 </Label>
+                                            //             {/* <List.Description>{team.projectdescription}</List.Description> */}
+                                            //         </List.Content>
+                                            //     </List.Item>
+                                            // </List>
+
+                                            <List>
+                                                <List.Item>
+                                                    <List.Content>
+                                                        <List.Header as='a'><List.Icon name='user' />{user.username}</List.Header>
+                                                        {/* <List.Description>{team.projectdescription}</List.Description> */}
+                                                    </List.Content>
+                                                </List.Item>
+                                            </List>
                                                 ))}
                                             </div>
                                     </Card>
