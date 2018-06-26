b13e508472e5c84574cabd02b65d7353a45c2821
Joshua Cook <jcook@lynxsolutions.com>
2018-06-26 16:53:48 -0700

    working startup

--- jaguar-client/src/containers/adminPages/template/TemplateView.js
+++ jaguar-client/src/containers/adminPages/template/TemplateView.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TaskToday from '../../taskview/TaskToday'
 import AppLayout from '../../layout/AppLayout'
 import NavSidebar from '../../layout/NavSidebar'
 import MainSidebar from '../../layout/MainSidebar'
 import OrgPageMain from './TemplateComponents/OrgPageMain.js'
+import decode from "jwt-decode";
+import { Query } from "react-apollo";
+import {Dimmer, Loader} from 'semantic-ui-react';
+import {userTaskDetails} from "../../apollo-graphql/userQueries";
+const token = localStorage.getItem('token');
 
 
-const TeamAdmin = () => {
-    return (
-        <AppLayout>
-            <NavSidebar />
-            <MainSidebar>
-                <TaskToday />
-            </MainSidebar>
-                <OrgPageMain />
-        </AppLayout>
-    )
-};
+class TeamAdmin extends Component {
+
+    render() {
+        const { user } = decode(token);
+        const variables = {_id: user._id};
+
+        return(
+            <Query query={userTaskDetails} variables={variables}>
+                { ({ loading, error, data }) => {
+                    if (loading) return (
+                        <div>
+                            <Dimmer active>
+                                <Loader />
+                            </Dimmer>
+                        </div>);
+                    if (error) return <p>Error :(</p>;
+                    return <div>
+                        <AppLayout>
+                            <NavSidebar/>
+                            <MainSidebar>
+                                <TaskToday
+                                    tasks={data.user.tasks}
+                                    defaultgroup={data.user.defaultgroup._id}
+                                    defaultproject={data.user.defaultproject._id}
+                                    defaultteam={data.user.defaultteam._id}
+                                    updateQuery={userTaskDetails}
+                                    variables={variables}
+                                />
+                            </MainSidebar>
+                            <OrgPageMain />
+                        </AppLayout>
+                    </div>;
+                }
+                }
+            </Query>
+        )
+    }
+}
+
+
 
 export default TeamAdmin;
